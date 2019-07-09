458f44c3aff8365f3e49a77bc0b42b317e36dc25
ashragai <ashragai@umail.ucsb.edu>
2019-07-09 16:38:55 -0400

    Add files via upload

--- ITC503.py
+++ ITC503.py
@@ -4,102 +4,100 @@ Created on Tue Oct  9 13:28:23 2018
 
 @author: Mak & Shan group
 """
-
+import re
 import numpy as np
 import visa
-rm=visa.ResourceManager()
+rm = visa.ResourceManager()
 
-class ITC503():
-    
-    def __init__(self):
+class ITC503():    
+    def __init__(self, address):  
+        self.address = address
         
-        self.inst=rm.open_resource('GPIB0::24::INSTR')
-        self.inst.write_termination='\r'
-        self.inst.read_termination='\r'
-
-    def R0(self):
-        while True:
-            self.r1=self.inst.query('R1')
-            self.r2=list(self.r1)
-            if (self.r2)[0]=='R':
-                break
-        self.r1=self.r1.strip('R')
-        return float(self.r1)
-    def R1(self):
-        while True:
-            self.r1=self.inst.query('R1')
-            self.r2=list(self.r1)
-            if (self.r2)[0]=='R':
-                break
-        self.r1=self.r1.strip('R')
-        return float(self.r1)
-
-    def R2(self):
-        while True:
-            self.r1=self.inst.query('R2')
-            self.r2=list(self.r1)
-            if (self.r2)[0]=='R':
-                break
-        self.r1=self.r1.strip('R')
-        return float(self.r1)
+    def connectInst(self):
+        try:
+            self.inst=rm.open_resource(self.address)
+            self.inst.baud_rate = 9600
+            self.inst.write_termination='\r'
+            self.inst.read_termination='\r'
+        except:
+            print 'Could not connect ITC503'
     
-    def R3(self):
-        self.r3=self.inst.query('R3')
-        self.r3=self.r1.strip('R')
-        return self.r3   
-
-    def R4(self):
-        self.r1=self.inst.query('R4')
-        self.r1=self.r1.strip('R')
-        return self.r1
-
-    def R5(self):
-        self.r2=self.inst.query('R5')
-        self.r2=self.r2.strip('R')
-        return self.r2
-
-    def R6(self):
-        self.r3=self.inst.query('R6')
-        self.r3=self.r3.strip('R')
-        return self.r3
+    def disconnectInst(self):
+        rm.close()
+        
+    def remote(self, unlocked = True):
+        '''
+        Sets the device to remote mode, either locked or unlocked (default unlocked)
+        '''
+        if unlocked:
+            self.inst.query('C3')
+        else:
+            self.inst.query('C1')
     
-    def R7(self):
-        self.r3=self.inst.query('R7')
-        self.r3=self.r3.strip('R')
-        return self.r3
-#setting the ITC503
+    def local(self, unlocked = True):
+        '''
+        Sets the device to local mode, either locked or unlocked (default unlocked)
+        '''
+        if unlocked:
+            self.inst.query('C2')
+        else:
+            self.inst.query('C0')
+            
+    def com_protocol(self, prot):
+        '''
+        Sets the communication protocol. Takes either 'NORM' to specify normal communication, or 'LF' to return <LF> after each <CR>
+        '''
+        if prot == 'NORM':
+            self.inst.query('Q0')
+        elif prot == 'LF':
+            self.inst.query('Q2')
+        else:
+            print "Protocol must be either 'NORM' or 'LF'"
+            
+    def get_temp(self, sensor):
+        '''
+        Read the temperature on the specified sensor (1, 2, or 3)
+        '''
+        if int(sensor) in [1, 2, 3]:
+            ans = self.inst.query('R{:d}'.format(sensor))
+            return float(ans.strip('R').strip('+'))
+        else:
+            print 'Seonsor must be either 1, 2, or 3'
+            
+    def get_set_temp(self):
+        '''
+        Returns the temperature setpoint
+        '''
+        ans = self.inst.query('R0')
+        return float(ans.strip('R').strip('+'))
+            
+    def set_heat_gas(self, heat, gas):
+        '''
+        Set the heater and gas to manual or auto. Accepts the setting for (heater, gas) has either AUTO or MAN: eg. query set_heat_gas('AUTO', 'MAN') to set the heat to auto and gas to manual mode
+        '''
+        setting = {'AUTO' : 1, 'MAN' : 0}
+        if (heat in setting.keys()) and (gas in setting.keys()):
+            num = setting[heat] * 2 + setting[gas]
+            self.inst.query('A{:d}'.format(num))
+        else:
+            print 'Heat and gas must be set to AUTO or MAN'
         
     def set_temp(self,setp):
-#        self.inst.write('A1')
-#        print(self.inst.read())
-        self.inst.write('T'+str(setp))
-        print(self.inst.read())
-    
-    def set_ned(self,setp):
-#        self.inst.write('A3')
-#        print(self.inst.read())
-        self.inst.write('G'+str(setp))
-        print(self.inst.read())
-
-    def FN(self,n):
-        self.inst.write('F'+str(n))
+        '''
+        Sets the tempature to the specified setpoint
+        '''
+        self.inst.query('T{:f}'.format(setp))
+        
+    def get_needle(self):
+        '''
+        Gets the gas flow in percent
+        '''
+        ans = self.inst.query('R7')
+        return float(ans.strip('R'))
         
     
-    
-    
-    
-    
-    
-#itc=ITC503()
-#
-#for i in range(100000):
-#    
-##itc.set_ned(10.0)
-#print(itc.inst.write('G15'))
-##print(itc.inst.query('V'))
-##itc.inst.write('C3')
-###itc.inst.write('O0.1')
-#print(itc.R2())
-##print(itc.R4())
-#itc.set_temp(70.00)
-#print(itc.R7())
+    def set_needle(self,setp):
+        '''
+        Sets the needle valve to the specified setpoint
+        '''
+        self.inst.query('G{:f}'.format(setp))
